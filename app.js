85b535b615b5dc881b49377b70653e65130da0ed
Juan <u202319404@upc.edu.pe>
2026-04-19 18:21:38 -0500

    feat: add images of all types

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("navbar__menu--open");
     });
 });
+
+const toolOptions = document.querySelectorAll(".tool-option");
+const toolPreviewImage = document.getElementById("toolPreviewImage");
+
+toolOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+        const newImage = option.getAttribute("data-image");
+        const newTitle = option.getAttribute("data-title");
+
+        toolPreviewImage.src = newImage;
+        toolPreviewImage.alt = newTitle;
+
+        toolOptions.forEach((item) => item.classList.remove("active"));
+        option.classList.add("active");
+    });
+});
